5f5d4461b3992e061cf2bddb465246e57f9bfe38
liu-stu <liuyuxin0415@163.com>
2022-11-02 22:44:00 +0800

    fix

--- Question001/function1.cpp
+++ Question001/function1.cpp
@@ -1,3 +1,17 @@
 vector<int> twoSum(vector<int>& nums, int target) {
-    
+	int length = nums.size();
+	vector<int> result;
+	vector<int>::iterator be = nums.begin();
+	int i = 0; int j;
+	bool isBreakLoop = true;
+
+	for (; i < length && isBreakLoop; i++) {
+		for (j = i + 1; j < length && isBreakLoop; j++)
+			if (*(be + i) + *(be + j) == target)
+				isBreakLoop = false;
+	}
+	result.push_back(--i);
+	result.push_back(--j);
+	
+	return result;
 }
